788a272eb33903ee5cb5cbabe55e88285c2b274a
Wesley Patterson <wpatt849@gmail.com>
2022-06-26 15:32:25 -0500

    added endpoint in server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const { response } = require('express')
 const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
 
@@ -21,6 +22,16 @@ app.use(express.static('public'))
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(cors())
+
+app.get('/', async(req,res) => {
+    try{
+        res.render('index.ejs')
+    }catch(error){
+        res.status(500).send({message: error.message})
+    }
+})
+
+
 
 app.listen(process.env.PORT || PORT, () => {
     console.log(`Server is running ${process.env.PORT}`)
