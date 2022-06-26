4f93a83ae08463ee54ce1fb3ef231a6845727ff2
Wesley Patterson <wpatt849@gmail.com>
2022-06-26 14:10:53 -0500

    added port and mongo connections

--- server.js
+++ server.js
@@ -15,3 +15,7 @@ MongoClient.connect(dbConnectionString)
         db = client.db(dbName)
         collection = db.collection('templateDocs')
     })    
+
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`Server is running ${process.env.PORT}`)
+})
